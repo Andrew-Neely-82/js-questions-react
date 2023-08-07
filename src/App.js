97c1a71b89e873dff3834e23d60fecb83f2f0712
Andrew-Neely-82 <andrew.neely82@gmail.com>
2023-08-06 20:34:17 -0500

    found a proper editor to use

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import './App.scss';
+import "./App.scss";
+import { Editor } from "@monaco-editor/react";
 
 function App() {
+  const editorOptions = {
+    fontFamily: "Courier New, monospace !important", // Set your desired font family here
+  };
+
+  const editorProps = {
+    height: "10rem",
+    width: "50%",
+    theme: "vs-dark",
+    defaultLanguage: "javascript",
+    options: editorOptions,
+  };
+
   return (
     <div className="App">
-
+      <h1>TEST test</h1>
+      <Editor {...editorProps} />
     </div>
   );
 }
